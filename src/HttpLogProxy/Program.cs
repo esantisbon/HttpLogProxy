2d2c1754f0b5d72c7c99433f1632c3786e14cc06
esantisbon <esantisbon@hotmail.com>
2022-10-10 23:35:03 -0500

    Ajuste para evitar advertencias de objectos IAsyncDisposable

--- src/HttpLogProxy/Program.cs
+++ src/HttpLogProxy/Program.cs
@@ -24,12 +24,6 @@ app.Map("/{**path}", async (HttpRequest request, IHttpClientFactory httpClientFa
     {
         return Results.BadRequest(error: "Storage not accesible!");
     }
-
-    await using var msZip = memoryStreamManager.GetStream();
-    var sb = new StringBuilder();
-    await using var outStream = new ZipOutputStream(msZip);
-    var context = request.HttpContext;
-    var requestMessage = new HttpRequestMessage();
     var httpClient = httpClientFactory.CreateClient();
     var ahora = DateTime.UtcNow;
     var targetUri = GetTargetUri(request);
@@ -37,115 +31,135 @@ app.Map("/{**path}", async (HttpRequest request, IHttpClientFactory httpClientFa
     {
         return Results.BadRequest(error: "Target URL missing or invalid!");
     }
-    await using var msReqBody = memoryStreamManager.GetStream();
-    sb.Append(request.Method).Append(" ").Append(targetUri.AbsoluteUri).Append(" ").Append(request.Protocol).AppendLine();
-    foreach (var head in request.Headers)
+    var context = request.HttpContext;
+    var sb = new StringBuilder();
+    HttpRequestMessage requestMessage = new();
+    HttpResponseMessage responseMessage;
+    var msZip = memoryStreamManager.GetStream();
+    var outStream = new ZipOutputStream(msZip);
+    var msReqBody = memoryStreamManager.GetStream();
+    await using (msReqBody.ConfigureAwait(false))
     {
-        foreach (string headerValue in head.Value)
+        sb.Append(request.Method).Append(" ").Append(targetUri.AbsoluteUri).Append(" ").Append(request.Protocol).AppendLine();
+        foreach (var head in request.Headers)
         {
-            sb.Append(head.Key);
-            sb.Append(": ");
-            sb.AppendLine(headerValue);
-        }
-    }
-    if (!HttpMethods.IsGet(request.Method)
-        && !HttpMethods.IsHead(request.Method)
-        && !HttpMethods.IsDelete(request.Method)
-        && !HttpMethods.IsTrace(request.Method))
-    {
-        await using (request.Body)
-        {
-            await request.Body.CopyToAsync(msReqBody).ConfigureAwait(false);
-        }
-        if (IsTextContentType(request?.ContentType))
-        {
-            sb.AppendLine();
-            sb.Append(Encoding.UTF8.GetString(msReqBody.ToArray()));
-        }
-        else
-        {
-            var zipEntryBinReq = new ZipEntry("request.bin")
+            foreach (string headerValue in head.Value)
             {
-                DateTime = ahora,
-                CompressionMethod = CompressionMethod.Deflated
-            };
-            await outStream.PutNextEntryAsync(zipEntryBinReq).ConfigureAwait(false);
-            msReqBody.Position = 0;
-            await msReqBody.CopyToAsync(outStream).ConfigureAwait(false);
-            await outStream.FlushAsync().ConfigureAwait(false);
+                sb.Append(head.Key);
+                sb.Append(": ");
+                sb.AppendLine(headerValue);
+            }
         }
-        msReqBody.Position = 0;
-        var streamContent = new StreamContent(msReqBody);
-        requestMessage.Content = streamContent;
-    }
-    var zipEntryReq = new ZipEntry("request.txt")
-    {
-        DateTime = ahora,
-        CompressionMethod = CompressionMethod.Deflated
-    };
-    await outStream.PutNextEntryAsync(zipEntryReq).ConfigureAwait(false);
-    outStream.Write(Encoding.UTF8.GetBytes(sb.ToString()));
-    await outStream.FlushAsync().ConfigureAwait(false);
-    sb.Clear();
-    foreach (var header in request.Headers)
-    {
-        requestMessage.Content?.Headers.TryAddWithoutValidation(header.Key, header.Value.ToArray());
-    }
-    requestMessage.RequestUri = targetUri;
-    requestMessage.Headers.Host = targetUri.Host;
-    requestMessage.Method = GetMethod(request.Method);
-    using var responseMessage = await httpClient.SendAsync(requestMessage, HttpCompletionOption.ResponseHeadersRead, context.RequestAborted);
-    foreach (var head in responseMessage.Headers)
-    {
-        foreach (string headerValue in head.Value)
+        if (!HttpMethods.IsGet(request.Method)
+            && !HttpMethods.IsHead(request.Method)
+            && !HttpMethods.IsDelete(request.Method)
+            && !HttpMethods.IsTrace(request.Method))
         {
-            sb.Append(head.Key);
-            sb.Append(": ");
-            sb.AppendLine(headerValue);
+            await using (request.Body)
+            {
+                await request.Body.CopyToAsync(msReqBody).ConfigureAwait(false);
+            }
+            if (IsTextContentType(request?.ContentType))
+            {
+                sb.AppendLine();
+                sb.Append(Encoding.UTF8.GetString(msReqBody.ToArray()));
+            }
+            else
+            {
+                var zipEntryBinReq = new ZipEntry("request.bin")
+                {
+                    DateTime = ahora,
+                    CompressionMethod = CompressionMethod.Deflated
+                };
+                await outStream.PutNextEntryAsync(zipEntryBinReq).ConfigureAwait(false);
+                msReqBody.Position = 0;
+                await msReqBody.CopyToAsync(outStream).ConfigureAwait(false);
+                await outStream.FlushAsync().ConfigureAwait(false);
+            }
+            msReqBody.Position = 0;
+            var streamContent = new StreamContent(msReqBody);
+            requestMessage.Content = streamContent;
         }
-    }
-    context.Response.StatusCode = (int)responseMessage.StatusCode;
-    await using var msRespBody = memoryStreamManager.GetStream();
-    using (var respStream = await responseMessage.Content.ReadAsStreamAsync().ConfigureAwait(false))
-    {
-        await respStream.CopyToAsync(msRespBody).ConfigureAwait(false);
-    }
-    string? contentType = responseMessage?.Content?.Headers?.ContentType?.MediaType;
-    if (IsTextContentType(contentType))
-    {
-        sb.AppendLine();
-        sb.Append(Encoding.UTF8.GetString(msRespBody.ToArray()));
-    }
-    else
-    {
-        var zipEntryBinResp = new ZipEntry("response.bin")
+        var zipEntryReq = new ZipEntry("request.txt")
         {
             DateTime = ahora,
             CompressionMethod = CompressionMethod.Deflated
         };
-        await outStream.PutNextEntryAsync(zipEntryBinResp).ConfigureAwait(false);
-        msRespBody.Position = 0;
-        await msRespBody.CopyToAsync(outStream).ConfigureAwait(false);
+        await outStream.PutNextEntryAsync(zipEntryReq).ConfigureAwait(false);
+        outStream.Write(Encoding.UTF8.GetBytes(sb.ToString()));
         await outStream.FlushAsync().ConfigureAwait(false);
+        sb.Clear();
+        if (request != null)
+        {
+            foreach (var header in request.Headers)
+            {
+                requestMessage.Content?.Headers.TryAddWithoutValidation(header.Key, header.Value.ToArray());
+            }
+            requestMessage.Method = GetMethod(request.Method);
+        }
+        requestMessage.RequestUri = targetUri;
+        requestMessage.Headers.Host = targetUri.Host;
+        responseMessage = await httpClient.SendAsync(requestMessage, HttpCompletionOption.ResponseHeadersRead, context.RequestAborted).ConfigureAwait(false);
     }
-    var zipEntryResp = new ZipEntry("response.txt")
+    var msRespBody = memoryStreamManager.GetStream();
+    await using (msZip.ConfigureAwait(false))
+    await using (outStream.ConfigureAwait(false))
+    await using (msRespBody.ConfigureAwait(false))
+    using (responseMessage)
     {
-        DateTime = ahora,
-        CompressionMethod = CompressionMethod.Deflated
-    };
-    await outStream.PutNextEntryAsync(zipEntryResp).ConfigureAwait(false);
-    outStream.Write(Encoding.UTF8.GetBytes(sb.ToString()));
-    sb.Clear();
-    await outStream.FlushAsync().ConfigureAwait(false);
-    await outStream.FinishAsync(default).ConfigureAwait(false);
-    msZip.Position = 0;
-    string container = NormalizaNombreContenedor(targetUri.Authority);
-    var blobContainer = new BlobContainerClient(storageCnnStr, container);
-    await blobContainer.CreateIfNotExistsAsync().ConfigureAwait(false);
-    string blobPath = $"{targetUri.AbsolutePath.Substring(0, Math.Min(targetUri.AbsolutePath.Length, 1000))}/{ahora:yyyyMMddhhmmssffff}.zip";
-    var blob = new BlobClient(storageCnnStr, container, blobPath);
-    await blob.UploadAsync(msZip).ConfigureAwait(false);
-    return Results.Bytes(msRespBody.ToArray(), contentType);
+
+        foreach (var head in responseMessage.Headers)
+        {
+            foreach (string headerValue in head.Value)
+            {
+                sb.Append(head.Key);
+                sb.Append(": ");
+                sb.AppendLine(headerValue);
+            }
+        }
+        context.Response.StatusCode = (int)responseMessage.StatusCode;
+        var respStream = await responseMessage.Content.ReadAsStreamAsync().ConfigureAwait(false);
+        await using (respStream.ConfigureAwait(false))
+        {
+            await respStream.CopyToAsync(msRespBody).ConfigureAwait(false);
+        }
+        string? contentType = responseMessage?.Content?.Headers?.ContentType?.MediaType;
+        if (IsTextContentType(contentType))
+        {
+            sb.AppendLine();
+            sb.Append(Encoding.UTF8.GetString(msRespBody.ToArray()));
+        }
+        else
+        {
+            ZipEntry? zipEntryBinResp = new ZipEntry("response.bin")
+            {
+                DateTime = ahora,
+                CompressionMethod = CompressionMethod.Deflated
+            };
+            await outStream.PutNextEntryAsync(zipEntryBinResp).ConfigureAwait(false);
+            msRespBody.Position = 0;
+            await msRespBody.CopyToAsync(outStream).ConfigureAwait(false);
+            await outStream.FlushAsync().ConfigureAwait(false);
+        }
+        var zipEntryResp = new ZipEntry("response.txt")
+        {
+            DateTime = ahora,
+            CompressionMethod = CompressionMethod.Deflated
+        };
+        await outStream.PutNextEntryAsync(zipEntryResp).ConfigureAwait(false);
+        outStream.Write(Encoding.UTF8.GetBytes(sb.ToString()));
+        sb.Clear();
+        await outStream.FlushAsync().ConfigureAwait(false);
+        await outStream.FinishAsync(default).ConfigureAwait(false);
+        msZip.Position = 0;
+        string container = NormalizaNombreContenedor(targetUri.Authority);
+        var blobContainer = new BlobContainerClient(storageCnnStr, container);
+        await blobContainer.CreateIfNotExistsAsync().ConfigureAwait(false);
+        string blobPath = $"{targetUri.AbsolutePath.Substring(0, Math.Min(targetUri.AbsolutePath.Length, 1000))}/{ahora:yyyyMMddhhmmssffff}.zip";
+        var blob = new BlobClient(storageCnnStr, container, blobPath);
+        await blob.UploadAsync(msZip).ConfigureAwait(false);
+        return Results.Bytes(msRespBody.ToArray(), contentType);
+    }
 });
 
 app.Run();
@@ -175,7 +189,7 @@ static string NormalizaNombreContenedor(string nombre)
     return sb.ToString(0, Math.Min(sb.Length, 63));
 }
 
-static Uri GetTargetUri(HttpRequest request)
+static Uri? GetTargetUri(HttpRequest request)
 {
     var paramToRemove = new HashSet<string>();
     string url = request.Query["url"];
